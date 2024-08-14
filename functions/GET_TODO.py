3152925f985cf21231ef544d8b30be98591d2cd5
Abdullah Bin Salman <90260374+MRABS03@users.noreply.github.com>
2024-08-14 17:52:44 +0500

    Delete functions directory

--- functions/GET_TODO.py
+++
@@ -1,7 +0,0 @@
-filepath= fr'..\files\todo.txt'
-
-
-def get_todos():
-    with open(filepath, 'r') as file:
-        todos = file.readlines()
-        return todos
--- functions/WRITE_TODO.py
+++
@@ -1,8 +0,0 @@
-from functions.GET_TODO import filepath
-
-filepath = fr'..\files\todo.txt'
-
-
-def write_todos(task):
-    with open(filepath, "w") as file:
-        file.writelines(task)
